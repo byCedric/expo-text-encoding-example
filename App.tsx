1ca22a4f27a7a8856eafafa061c57e01f3c4058b
Test User <github@cedric.dev>
2024-01-09 13:22:53 +0100

    feature: add text encoder/decoder polyfills

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
+import 'effect'; // Notice this import, which fails without global text encoder/decoder
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+// Validating these encoders are available
+console.log('Checking Text Encoder/Decoder', {
+  TextDecoder: global.TextDecoder,
+  TextEncoder: global.TextEncoder,
+});
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>TextEncoder = {typeof TextEncoder}</Text>
+      <Text>TextDecoder = {typeof TextDecoder}</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ index.ts
@@ -0,0 +1,6 @@
+import 'fast-text-encoding'; // This is the polyfill you'll need
+import { registerRootComponent } from 'expo';
+
+import App from './App';
+
+registerRootComponent(App);
